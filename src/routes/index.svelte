bd02e924bac70563eda9edfd639c57171d892ba4
Christian Potdevin <christian@qrador.com>
2022-03-21 14:56:28 -0500

    feat: set logo as link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 
 <div class="shadow">
 	<div class="mx-auto w-full max-w-screen-xl px-2 py-5">
-		<h2 class="text-xl font-semibold text-teal-500">ptd.vin</h2>
+		<a class="text-xl font-semibold text-teal-500" href="/">ptd.vin</a>
 	</div>
 </div>
 
