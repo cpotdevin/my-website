1eb53e36ff4fd3fde0ea8f02c1b2f4b802e78c69
Christian Potdevin <christian@qrador.com>
2022-03-21 14:34:28 -0500

    feat: initial setup

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<div class="shadow">
+	<div class="mx-auto w-full max-w-screen-xl px-2 py-5">
+		<h2 class="text-xl font-semibold text-teal-500">ptd.vin</h2>
+	</div>
+</div>
+
+<div class="mx-auto w-full max-w-screen-xl px-2">
+	<div class="my-20 lg:my-36 lg:mx-36">
+		<h1
+			class="w-fit bg-gradient-to-r from-teal-500 via-violet-500 to-pink-500 bg-clip-text text-4xl font-bold text-transparent md:text-6xl"
+		>
+			Christian Potdevin
+		</h1>
+		<p class="mt-2 text-2xl text-gray-600 lg:ml-6">
+			Welcome to my <span class="underline">personal</span> website.
+		</p>
+	</div>
+</div>
