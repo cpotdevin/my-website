8ef79654c2802e4349c3cd2c9254ade57dc60d7d
Christian Potdevin <christian@qrador.com>
2022-04-10 20:11:27 -0500

    style: use spaces instead of tabs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Header from '$lib/Components/Header.svelte';
+  import Header from '$lib/Components/Header.svelte';
 </script>
 
 <svelte:head><title>ptd.vin</title></svelte:head>
@@ -7,14 +7,14 @@
 <Header />
 
 <div class="mx-auto w-full max-w-screen-xl px-2">
-	<div class="my-20 lg:my-36 lg:mx-36">
-		<h1
-			class="w-fit bg-gradient-to-r from-teal-500 via-violet-500 to-pink-500 bg-clip-text text-4xl font-bold text-transparent md:text-6xl"
-		>
-			Christian Potdevin
-		</h1>
-		<p class="mt-2 text-2xl text-gray-600 lg:ml-6">
-			Welcome to my <span class="underline">personal</span> website
-		</p>
-	</div>
+  <div class="my-20 lg:my-36 lg:mx-36">
+    <h1
+      class="w-fit bg-gradient-to-r from-teal-500 via-violet-500 to-pink-500 bg-clip-text text-4xl font-bold text-transparent md:text-6xl"
+    >
+      Christian Potdevin
+    </h1>
+    <p class="mt-2 text-2xl text-gray-600 lg:ml-6">
+      Welcome to my <span class="underline">personal</span> website
+    </p>
+  </div>
 </div>
--- src/routes/webgl/hello-world.svelte
+++ src/routes/webgl/hello-world.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import Header from '$lib/Components/Header.svelte';
+  import Header from '$lib/Components/Header.svelte';
 
-	import { onMount } from 'svelte';
+  import { onMount } from 'svelte';
 
-	let canvas;
+  let canvas;
 
-	var vertexShaderSource = `#version 300 es
+  var vertexShaderSource = `#version 300 es
 
 // an attribute is an input (in) to a vertex shader.
 // It will receive data from a buffer
@@ -30,7 +30,7 @@ void main() {
 }
 `;
 
-	var fragmentShaderSource = `#version 300 es
+  var fragmentShaderSource = `#version 300 es
 
 // fragment shaders don't have a default precision so we need
 // to pick one. highp is a good default. It means "high precision"
@@ -47,153 +47,153 @@ void main() {
 }
 `;
 
-	function createShader(gl: WebGLRenderingContext, type: number, source: string) {
-		var shader = gl.createShader(type);
-		gl.shaderSource(shader, source);
-		gl.compileShader(shader);
-		var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-		if (success) {
-			return shader;
-		}
+  function createShader(gl: WebGLRenderingContext, type: number, source: string) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+      return shader;
+    }
 
-		console.log(gl.getShaderInfoLog(shader));
-		gl.deleteShader(shader);
-	}
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+  }
 
-	function createProgram(
-		gl: WebGLRenderingContext,
-		vertexShader: WebGLShader,
-		fragmentShader: WebGLShader,
-	) {
-		var program = gl.createProgram();
-		gl.attachShader(program, vertexShader);
-		gl.attachShader(program, fragmentShader);
-		gl.linkProgram(program);
-		var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-		if (success) {
-			return program;
-		}
+  function createProgram(
+    gl: WebGLRenderingContext,
+    vertexShader: WebGLShader,
+    fragmentShader: WebGLShader,
+  ) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+      return program;
+    }
 
-		console.log(gl.getProgramInfoLog(program));
-		gl.deleteProgram(program);
-	}
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+  }
 
-	function resizeCanvasToDisplaySize(canvas, multiplier = 1) {
-		const width = (canvas.clientWidth * multiplier) | 0;
-		const height = (canvas.clientHeight * multiplier) | 0;
-		if (canvas.width !== width || canvas.height !== height) {
-			canvas.width = width;
-			canvas.height = height;
-			return true;
-		}
-		return false;
-	}
+  function resizeCanvasToDisplaySize(canvas, multiplier = 1) {
+    const width = (canvas.clientWidth * multiplier) | 0;
+    const height = (canvas.clientHeight * multiplier) | 0;
+    if (canvas.width !== width || canvas.height !== height) {
+      canvas.width = width;
+      canvas.height = height;
+      return true;
+    }
+    return false;
+  }
 
-	function randomInt(range: number) {
-		return Math.floor(Math.random() * range);
-	}
+  function randomInt(range: number) {
+    return Math.floor(Math.random() * range);
+  }
 
-	function setRectangle(
-		gl: WebGLRenderingContext,
-		x: number,
-		y: number,
-		width: number,
-		height: number,
-	) {
-		var x1 = x;
-		var x2 = x + width;
-		var y1 = y;
-		var y2 = y + height;
+  function setRectangle(
+    gl: WebGLRenderingContext,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+  ) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
 
-		// NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
-		// whatever buffer is bound to the `ARRAY_BUFFER` bind point
-		// but so far we only have one buffer. If we had more than one
-		// buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+    // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
+    // whatever buffer is bound to the `ARRAY_BUFFER` bind point
+    // but so far we only have one buffer. If we had more than one
+    // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
 
-		gl.bufferData(
-			gl.ARRAY_BUFFER,
-			new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
-			gl.STATIC_DRAW,
-		);
-	}
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
+      gl.STATIC_DRAW,
+    );
+  }
 
-	onMount(() => {
-		if (canvas instanceof HTMLCanvasElement) {
-			const gl = canvas.getContext('webgl2');
+  onMount(() => {
+    if (canvas instanceof HTMLCanvasElement) {
+      const gl = canvas.getContext('webgl2');
 
-			if (!gl) {
-				return;
-			}
+      if (!gl) {
+        return;
+      }
 
-			const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-			const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+      const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+      const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
-			const program = createProgram(gl, vertexShader, fragmentShader);
+      const program = createProgram(gl, vertexShader, fragmentShader);
 
-			const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-			const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
-			var colorLocation = gl.getUniformLocation(program, 'u_color');
+      const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+      const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
+      var colorLocation = gl.getUniformLocation(program, 'u_color');
 
-			const positionBuffer = gl.createBuffer();
+      const positionBuffer = gl.createBuffer();
 
-			gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-			const positions = [10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30];
-			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+      const positions = [10, 20, 80, 20, 10, 30, 10, 30, 80, 20, 80, 30];
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-			const vao = gl.createVertexArray();
-			gl.bindVertexArray(vao);
+      const vao = gl.createVertexArray();
+      gl.bindVertexArray(vao);
 
-			gl.enableVertexAttribArray(positionAttributeLocation);
+      gl.enableVertexAttribArray(positionAttributeLocation);
 
-			const size = 2; // 2 components per iteration
-			const type = gl.FLOAT; // the data is 32bit floats
-			const normalize = false; // don't normalize the data
-			const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
-			let offset = 0; // start at the beginning of the buffer
-			gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+      const size = 2; // 2 components per iteration
+      const type = gl.FLOAT; // the data is 32bit floats
+      const normalize = false; // don't normalize the data
+      const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+      let offset = 0; // start at the beginning of the buffer
+      gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
 
-			resizeCanvasToDisplaySize(gl.canvas);
+      resizeCanvasToDisplaySize(gl.canvas);
 
-			gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-			// Clear the canvas
-			gl.clearColor(0, 0, 0, 0);
-			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+      // Clear the canvas
+      gl.clearColor(0, 0, 0, 0);
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-			gl.useProgram(program);
-			gl.bindVertexArray(vao);
+      gl.useProgram(program);
+      gl.bindVertexArray(vao);
 
-			gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+      gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
-			for (var ii = 0; ii < 50; ++ii) {
-				// Setup a random rectangle
-				setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
+      for (var ii = 0; ii < 50; ++ii) {
+        // Setup a random rectangle
+        setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
 
-				// Set a random color.
-				gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
+        // Set a random color.
+        gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
 
-				// Draw the rectangle.
-				var primitiveType = gl.TRIANGLES;
-				offset = 0;
-				var count = 6;
-				gl.drawArrays(primitiveType, offset, count);
-			}
-		}
-	});
+        // Draw the rectangle.
+        var primitiveType = gl.TRIANGLES;
+        offset = 0;
+        var count = 6;
+        gl.drawArrays(primitiveType, offset, count);
+      }
+    }
+  });
 </script>
 
 <Header />
 
 <div class="mx-auto w-full max-w-screen-md px-2">
-	<div class="my-10">
-		<h1
-			class="w-fit bg-gradient-to-r from-teal-500 via-violet-500 to-pink-500 bg-clip-text py-1 text-4xl font-bold text-transparent"
-		>
-			Testing out WebGL
-		</h1>
-		<h2 class="text-gray-500">2022-03-21</h2>
-	</div>
+  <div class="my-10">
+    <h1
+      class="w-fit bg-gradient-to-r from-teal-500 via-violet-500 to-pink-500 bg-clip-text py-1 text-4xl font-bold text-transparent"
+    >
+      Testing out WebGL
+    </h1>
+    <h2 class="text-gray-500">2022-03-21</h2>
+  </div>
 
-	<canvas bind:this={canvas} class="mx-auto rounded-lg border border-gray-300" />
+  <canvas bind:this={canvas} class="mx-auto rounded-lg border border-gray-300" />
 </div>
