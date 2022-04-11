ad638cebd5025dfd87d049ee1c08f0f82e24f85e
Christian Potdevin <christian@qrador.com>
2022-04-10 20:16:29 -0500

    style: use spaces instead of tabs

--- src/lib/Components/Header.svelte
+++ src/lib/Components/Header.svelte
@@ -1,5 +1,5 @@
 <div class="shadow">
-	<div class="mx-auto w-full max-w-screen-xl px-2 py-5">
-		<a class="text-xl font-semibold text-teal-500" href="/">ptd.vin</a>
-	</div>
+  <div class="mx-auto w-full max-w-screen-xl px-2 py-5">
+    <a class="text-xl font-semibold text-teal-500" href="/">ptd.vin</a>
+  </div>
 </div>
