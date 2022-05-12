10a80e9688881f6607d99f32dbd93b2845a3a4a2
Christian Potdevin <chrpotdevin@gmail.com>
2022-05-11 21:03:49 -0500

    feat: update fade timing

--- src/lib/components/RealTimeSketchPad.svelte
+++ src/lib/components/RealTimeSketchPad.svelte
@@ -16,7 +16,7 @@
     }
 
     let interval = setInterval(() => {
-      context.fillStyle = 'rgba(255,255,255,0.3)';
+      context.fillStyle = 'rgba(255,255,255,0.1)';
       context.fillRect(0, 0, canvas.width, canvas.height);
     }, 100);
 
